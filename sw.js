12f1ca5d90b3df4f483aaaefe6b72072b4079e02
Anna Kolodiy <annakolodiy@mac.mynetworksettings.com>
2025-09-05 22:13:41 -0400

    First commit - budget app

---
+++ sw.js
@@ -0,0 +1,45 @@
+// sw.js - simple service worker for offline caching
+
+const CACHE_NAME = "budget-app-cache-v1";
+const FILES_TO_CACHE = [
+  "./",
+  "./budget.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+// Install Service Worker
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate Service Worker
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch cached files
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
